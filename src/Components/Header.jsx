2165eca3453c9777c5154d58861737e63101ae17
Yury Shapkarin <yury@shapkarin.me>
2017-06-09 17:01:02 +0300

    jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router';
 
 export default class Header extends Component {
   render() {
-    return <nav className={`navbar  ${this.props.name === 'Landing' ? 'navbar-default' : 'navbar-inverse'}`}>
+    return <nav className={`navbar ${this.props.name === 'Landing' ? 'navbar-default' : 'navbar-inverse'}`}>
       <div className="container-fluid">
         <div className="navbar-header">
           <Link className="navbar-brand" to={this.props.name === 'Landing' ? '/' : '/posts'}>
