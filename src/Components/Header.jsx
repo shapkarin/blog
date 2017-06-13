6a64d29fdab0f72734b9af43eeac684b80c6e430
Yury Shapkarin <yury@shapkarin.me>
2017-06-13 14:28:02 +0300

    stateless component <Header />

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,20 +3,20 @@ import PropTypes from 'prop-types';
 
 import { Link } from 'react-router';
 
-export default class Header extends Component {
-  render() {
-    return <nav className={`navbar ${this.props.name === 'Landing' ? 'navbar-default' : 'navbar-inverse'}`}>
+const Header = ({name}) => (
+    <nav className={`navbar ${name === 'Landing' ? 'navbar-default' : 'navbar-inverse'}`}>
       <div className="container-fluid">
         <div className="navbar-header">
-          <Link className="navbar-brand" to={this.props.name === 'Landing' ? '/' : '/posts'}>
-            {this.props.name}
+          <Link className="navbar-brand" to={name === 'Landing' ? '/' : '/posts'}>
+            {name}
           </Link>
         </div>
       </div>
-    </nav>;
-  }
-}
+    </nav>
+)
 
 Header.propTypes = {
   name: PropTypes.string.isRequired
 };
+
+export default Header;
