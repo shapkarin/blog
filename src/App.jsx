ba5c40cf26d7b22060bfb16e72f066d17d72f6db
Yury Shapkarin <yury@shapkarin.me>
2017-06-13 14:23:47 +0300

    stateless component app component

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import Router from './Router';
 
 import './Layout/Style/animation.css';
 
-class App extends Component {
-  render() {
+const App = () => {
     return <Router />;
-  }
 }
 
 export default App;
