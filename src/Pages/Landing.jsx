b4c3fcd511f32e48e13cd2c53f75eee27d9a3adc
Yury Shapkarin <yury@shapkarin.me>
2017-07-05 18:27:49 +0300

    stateless component

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 import { Link } from 'react-router';
 
-export default class LandingPage extends Component {
-  render() {
-    return <div className="jumbotron">
+const Header = () => (
+    <div className="jumbotron">
       <h1>Hello, world!</h1>
       <p><Link to="posts" className="btn btn-primary btn-lg" role="button">Блог</Link></p>
-    </div>;
-  }
-}
+    </div>
+)
+
+export default Header;
